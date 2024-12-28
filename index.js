cd5a2654d6b821c554019ef561d71de10f9a7cb6
naomiyaki <hello@pumpkin.ink>
2024-12-28 13:35:04 -0800

    [F] Add support for local testing with .env

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+// Get local testing data from .env if it exists
+const local = process.env.LOCAL_TEST ? process.env.LOCAL_TEST : false;
+const localData = {
+    "api-url": process.env.URL,
+    "api-key": process.env.KEY,
+    tag: process.env.TAG,
+    field: process.env.FIELD,
+    value: process.env.VALUE,
+    days: process.env.DAYS,
+};
+
 /* eslint-disable no-console */
-const core = require('@actions/core');
-const GhostAdminApi = require('@tryghost/admin-api');
+if (local) {
+    console.log("Running with local config");
+} else {
+    console.log("Running as action");
+}
+
+const core = require("@actions/core");
+const GhostAdminApi = require("@tryghost/admin-api");
+
+// Get either the local or remote value
+// depending on configuration
+const getConfigData = (name) => {
+    if (local && localData[name]) return localData[name];
+    return core.getInput(name);
+};
 
 // Convert boolean strings to true booleans
 const getValue = () => {
-    let value = core.getInput('value');
+    let value = getConfigData("value");
 
-    if (value === 'true') {
+    if (value === "true") {
         value = true;
-    } else if (value === 'false') {
+    } else if (value === "false") {
         value = false;
     }
 
@@ -25,34 +49,42 @@ const calculateDaysSince = (date) => {
 (async function main() {
     try {
         const api = new GhostAdminApi({
-            url: core.getInput('api-url'),
-            key: core.getInput('api-key'),
-            version: 'canary'
+            url: getConfigData("api-url"),
+            key: getConfigData("api-key"),
+            version: "canary",
         });
 
-        const tag = core.getInput('tag');
-        const field = core.getInput('field');
+        const tag = getConfigData("tag");
+        const field = getConfigData("field");
         const value = getValue();
-        const days = core.getInput('days');
+        const days = getConfigData("days");
 
-        const posts = await api.posts.browse({filter: `tag:${tag}`});
+        const posts = await api.posts.browse({ filter: `tag:${tag}` });
 
-        await Promise.all(posts.map(async (post) => {
-            const differenceInDays = calculateDaysSince(post.published_at);
+        await Promise.all(
+            posts.map(async (post) => {
+                const differenceInDays = calculateDaysSince(post.published_at);
 
-            console.log(`Post "${post.title}" published ${differenceInDays} days ago`);
+                console.log(
+                    `Post "${post.title}" published ${differenceInDays} days ago`
+                );
 
-            // If enough days have passed, we will update the post
-            if (differenceInDays > days) {
-                post[field] = value;
-                console.log(`Updating post "${post.title}"`);
-                await api.posts.edit(post);
-            } else {
-                console.log(`Not updating post "${post.title}", ${days - differenceInDays + 1} days to go`);
-            }
-        }));
+                // If enough days have passed, we will update the post
+                if (differenceInDays > days) {
+                    post[field] = value;
+                    console.log(`Updating post "${post.title}"`);
+                    await api.posts.edit(post);
+                } else {
+                    console.log(
+                        `Not updating post "${post.title}", ${
+                            days - differenceInDays + 1
+                        } days to go`
+                    );
+                }
+            })
+        );
     } catch (err) {
         console.error(err);
         process.exit(1);
     }
-}());
+})();
