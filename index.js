43f2b89671ea42b9a136ef7494de0959c7ee75d1
naomiyaki <hello@pumpkin.ink>
2024-12-28 19:00:54 -0800

    [F] Setup initial newsletter functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const localData = {
     "api-url": process.env.URL,
     "api-key": process.env.KEY,
     tag: process.env.TAG,
+    tagPost: process.env.TAG_POST,
     field: process.env.FIELD,
     value: process.env.VALUE,
     days: process.env.DAYS,
@@ -59,28 +60,73 @@ const calculateDaysSince = (date) => {
         const value = getValue();
         const days = getConfigData("days");
 
-        const posts = await api.posts.browse({ filter: `tag:${tag}` });
+        // Get all posts matching the tag
+        const matchingPosts = await api.posts.browse({
+            filter: `tag:${tag}+status:published`,
+        });
+        // Pair down posts to only ones that are past the days
+        // configured by the workflow
+        const posts = matchingPosts.filter((post, index) => {
+            const differenceInDays = calculateDaysSince(post.published_at);
+
+            console.log(
+                `Post "${post.title}" published ${differenceInDays} days ago`
+            );
+
+            if (differenceInDays > days) {
+                console.log(`Post ${post.title} is ready to be made public!`);
+                return post;
+            }
+
+            console.log(
+                `Post ${post.title} has ${
+                    days - differenceInDays + 1
+                } day(s) to go.`
+            );
+            return false;
+        });
+
+        if (posts.length === 0) {
+            console.log("No posts to be updated.");
+        }
+
+        // Save the post IDs for
+        // republishing later
+        const postIDs = posts.map((post) => post.id);
 
         await Promise.all(
             posts.map(async (post) => {
-                const differenceInDays = calculateDaysSince(post.published_at);
-
+                // Assign the new field value from
+                // the workflow
+                post[field] = value;
+                // Set the post status to draft to "unpublish it"
+                post["status"] = "draft";
                 console.log(
-                    `Post "${post.title}" published ${differenceInDays} days ago`
+                    `Updating post "${post.title}" ${field} to ${value}`
                 );
+                // Edit the posts and unpublish them
+                await api.posts.edit(post);
+            })
+        );
 
-                // If enough days have passed, we will update the post
-                if (differenceInDays > days) {
-                    post[field] = value;
-                    console.log(`Updating post "${post.title}"`);
-                    await api.posts.edit(post);
-                } else {
-                    console.log(
-                        `Not updating post "${post.title}", ${
-                            days - differenceInDays + 1
-                        } days to go`
-                    );
-                }
+        const idFilter = postIDs.map((id) => `id:${id}`).join(",");
+        // Get the posts that were just unpublished with the saved IDs
+        // Ensure only draft posts are used
+        const unpublishedPosts = await api.posts.browse({
+            filter: `${idFilter}+status:draft`,
+        });
+
+        await Promise.all(
+            unpublishedPosts.map(async (post) => {
+                post["status"] = "published";
+                console.log(
+                    `Republishing post "${post.title}" with announcement`
+                );
+                // Edit the posts and unpublish them
+                await api.posts.edit(post, {
+                    newsletter: "default-newsletter",
+                    email_segment: "status:free",
+                });
             })
         );
     } catch (err) {
